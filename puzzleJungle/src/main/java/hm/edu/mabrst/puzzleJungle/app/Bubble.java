70a0e7c01b97a4b0e3c0989ebdb4626b46212235
felix B. <brassfelix@googlemail.com>
2012-11-27 15:38:42 +0100

    commit von iwas???

--- puzzleJungle/src/main/java/hm/edu/mabrst/puzzleJungle/app/Bubble.java
+++ puzzleJungle/src/main/java/hm/edu/mabrst/puzzleJungle/app/Bubble.java
@@ -96,17 +96,6 @@ public class Bubble implements IBubbleShooter, IBubbleHandler {
 		neighbors = id;
 	}
 
-	public boolean gotNeighbor() {
-		int i = 0;
-		for (int d = 0; d < 6; d++) {
-			i = i + getNeighbors()[d];
-		}
-		if (i > 0)
-			return true;
-		else
-			return false;
-	}
-
 	public int getId() {
 		return id;
 	}
